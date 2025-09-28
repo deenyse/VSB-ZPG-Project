3b1cba871c93b0b20fc28ec9ba2fe34d0c2403bc
deenyse <denvery31@gmail.com>
2025-09-28 20:39:39 +0200

    CV_2 task complete

--- ZPG_CV_1/Triangle.h
+++ ZPG_CV_1/Triangle.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "Model.h"
 
 
@@ -6,12 +6,12 @@ class Triangle : public Model
 {
 private:
 	// Coordinates		//Colors
-	float points[36] = {
-		0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-	   -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-		
+	float points[18] = {
+	-0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // brm l
+	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, // btm r
+	 0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f  // top
 	};
+
 public:
 	void create() override;
 	void draw() override;
