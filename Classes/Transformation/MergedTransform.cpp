4317140ea408259ffecc6bc25faf6d47d9fac4c5
deenyse <denvery31@gmail.com>
2025-11-17 00:18:28 +0100

    Ref: FollowingLight updates only on transformation change.

--- Classes/Transformation/MergedTransform.cpp
+++
@@ -1,8 +0,0 @@
-#include "MergedTransform.h"
-
-
-MergedTransform::MergedTransform(glm::mat4 mergedMatrix) : mergedTransforms(mergedMatrix) {};
-
-glm::mat4 MergedTransform::getMatrix() {
-    return mergedTransforms;
-}
